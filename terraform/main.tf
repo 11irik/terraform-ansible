cf5029f3167f23efb3753cfcbb8b7fb20f2399e1
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-03-28 07:35:34 +0400

    WINWINWIN

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,30 +13,108 @@ provider "google" {
   zone        = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+# VPC network
+resource "google_compute_network" "ilb_network" {
+  name                    = "l7-ilb-network"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_firewall" "ssh-rule" {
-  name = "ssh-rule"
-  network = google_compute_network.vpc_network.name
+resource "google_compute_firewall" "fw-rule" {
+  name = "terraform-fw-rule"
+  network = google_compute_network.ilb_network.name
   allow {
     protocol = "tcp"
-    ports = ["22"]
-  }
-  target_tags = ["dev"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "http-rule" {
-  name = "http-rule"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "tcp"
-    ports = ["80"]
+    ports = ["22", "80"]
   }
   target_tags = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
+}
+
+# proxy-only subnet
+resource "google_compute_subnetwork" "proxy_subnet" {
+  name          = "l7-ilb-proxy-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = google_compute_network.ilb_network.id
+}
+
+# backend subnet
+resource "google_compute_subnetwork" "ilb_subnet" {
+  name          = "l7-ilb-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.ilb_network.id
+}
+
+# forwarding rule
+resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
+  name                  = "l7-ilb-forwarding-rule"
+  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network               = google_compute_network.ilb_network.id
+  subnetwork            = google_compute_subnetwork.ilb_subnet.id
+  network_tier          = "PREMIUM"
+}
+
+# HTTP target proxy
+resource "google_compute_region_target_http_proxy" "default" {
+  name     = "l7-ilb-target-http-proxy"
+  url_map  = google_compute_region_url_map.default.id
+}
+
+# URL map
+resource "google_compute_region_url_map" "default" {
+  name            = "l7-ilb-regional-url-map"
+  default_service = google_compute_region_backend_service.default.id
+}
+
+# backend service
+resource "google_compute_region_backend_service" "default" {
+  name                  = "l7-ilb-backend-subnet"
+  protocol              = "HTTP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  timeout_sec           = 10
+  health_checks         = [google_compute_region_health_check.default.id]
+  backend {
+    group           = google_compute_instance_group.servers.id
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
+# health check
+resource "google_compute_region_health_check" "default" {
+  name     = "l7-ilb-hc"
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
+  }
+}
+
+# allow all access from IAP and health check ranges
+resource "google_compute_firewall" "fw-iap" {
+  name          = "l7-ilb-fw-allow-iap-hc"
+  direction     = "INGRESS"
+  network       = google_compute_network.ilb_network.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
+  }
+}
+
+# allow http from proxy subnet to backends
+resource "google_compute_firewall" "fw-ilb-to-backends" {
+  name          = "l7-ilb-fw-allow-ilb-to-backends"
+  direction     = "INGRESS"
+  network       = google_compute_network.ilb_network.id
+  source_ranges = ["10.0.0.0/24"]
+  target_tags   = ["http-server"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080"]
+  }
 }
 
 resource "google_compute_instance" "vm_instance" {
@@ -51,7 +129,8 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
     access_config {
     }
   }
@@ -69,7 +148,8 @@ resource "google_compute_instance" "vm_instance_2" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
     access_config {
     }
   }
@@ -87,9 +167,34 @@ resource "google_compute_instance" "vm_instance_3" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
     access_config {
     }
   }
 }
 
+resource "google_compute_instance_group" "servers" {
+  name        = "terraform-servers"
+
+  instances = [
+    google_compute_instance.vm_instance.id,
+    google_compute_instance.vm_instance_2.id,
+    google_compute_instance.vm_instance_3.id
+  ]
+}
+
+# test instance
+resource "google_compute_instance" "vm-test" {
+  name         = "l7-ilb-test-vm"
+  machine_type = "e2-small"
+  network_interface {
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+}
